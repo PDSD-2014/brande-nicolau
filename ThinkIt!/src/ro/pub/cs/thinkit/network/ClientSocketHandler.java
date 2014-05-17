4857d34ac61cda18d47fdd2cc18e982b1692cdfb
Adrian Nicolau <adrian.nicolau74@gmail.com>
2014-05-17 12:12:11 +0300

    Improve skeleton

---
+++ ThinkIt!/src/ro/pub/cs/thinkit/network/ClientSocketHandler.java
@@ -0,0 +1,49 @@
+package ro.pub.cs.thinkit.network;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import ro.pub.cs.thinkit.gui.WiFiServiceDiscoveryActivity;
+import android.os.Handler;
+import android.util.Log;
+
+public class ClientSocketHandler extends Thread {
+
+    private static final String TAG = "ClientSocketHandler";
+    private Handler handler;
+    private NetworkManager chat;
+    private InetAddress mAddress;
+
+    public ClientSocketHandler(Handler handler, InetAddress groupOwnerAddress) {
+	this.handler = handler;
+	this.mAddress = groupOwnerAddress;
+    }
+
+    @Override
+    public void run() {
+	Socket socket = new Socket();
+	try {
+	    socket.bind(null);
+	    socket.connect(new InetSocketAddress(mAddress.getHostAddress(), WiFiServiceDiscoveryActivity.SERVER_PORT),
+		    5000);
+	    Log.d(TAG, "Launching the I/O handler");
+	    chat = new NetworkManager(socket, handler);
+	    new Thread(chat).start();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    try {
+		socket.close();
+	    } catch (IOException e1) {
+		e1.printStackTrace();
+	    }
+	    return;
+	}
+    }
+
+    public NetworkManager getChat() {
+	return chat;
+    }
+
+}
